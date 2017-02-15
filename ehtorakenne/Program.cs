bb7bb11371235f92f4764ca719c73e7ba7d3587e
vartovi <viljo.varto@gmail.com>
2017-02-15 09:40:36 +0200

    Added comments

--- ehtorakenne/Program.cs
+++ ehtorakenne/Program.cs
@@ -10,88 +10,90 @@ namespace ehtorakenne
     {
         static void Main(string[] args)
         {
-            //Ask user for input
-            Console.Write("Anna luku: ");
+            //Task 1. Ask for number and check if number is positive or negative
+            Console.Write("Type number ");
             string userInput = Console.ReadLine();
             int numero;
-            //Check if input is a number
+            //Check if input is a number, psoitive or negative.
             if (int.TryParse(userInput, out numero))
             {
                 if (numero == 0)
                 {
-                    Console.WriteLine("Antamasi luku on 0");
+                    Console.WriteLine("Your number is 0");
                 }
                 if (numero > 0)
                 {
-                    Console.WriteLine("Numero " + userInput + " on positiivinen.");
+                    Console.WriteLine("Number " + userInput + " is positive.");
                 }
                 if (numero < 0)
                 {
-                    Console.WriteLine("Numero " + userInput + " on negatiivinen.");
+                    Console.WriteLine("Number " + userInput + " is negative.");
                 }
             }
             else
             {
-                Console.WriteLine("Et antanut numeroa");
+                Console.WriteLine("You didn't write a number");
             }
 
-            Console.Write("\nAnna uusi luku: ");
+            //Task 2. Ask for a number and check if its positive or negative
+            Console.Write("\nType new number: ");
             string userInput2 = Console.ReadLine();
             if (int.TryParse(userInput2, out numero))
             {
                 if (numero % 2 == 0 || numero == 0)
                 {
-                    Console.WriteLine("Antamasi luku " + numero + " on parillinen.");
+                    Console.WriteLine("Number " + numero + " is even.");
                 }
                 else
                 {
-                    Console.WriteLine("Antamasi luku " + numero + " on pariton.");
+                    Console.WriteLine("Number " + numero + " is odd.");
                 }
                 
             }
             else
             {
-                Console.WriteLine("Et antanut numeroa");
+                Console.WriteLine("You didn't write a number");
             }
-
-            Console.Write("\nAnna uusi luku: ");
+            //Task 3. Combined tasks 1 and 2
+            Console.Write("\nWrite new number: ");
             string userInput3 = Console.ReadLine();
             if (int.TryParse(userInput3, out numero))
             {
                 if (numero == 0)
                 {
-                    Console.WriteLine("Antamasi luku on 0");
+                    Console.WriteLine("Number is 0");
                 }
                 if (numero > 0)
                 {
                     if (numero % 2 == 0)
                     {
-                        Console.WriteLine("Antamasi luku " + numero + " on positiivinen ja parillinen.");
+                        Console.WriteLine("Number " + numero + " is positíve and even.");
                     }
                     else
                     {
-                        Console.WriteLine("Antamasi luku " + numero + " on positiivinen ja pariton.");
+                        Console.WriteLine("Number " + numero + " is positive and odd.");
                     }
                 }
                 if (numero < 0)
                 {
                     if (numero % 2 == 0)
                     {
-                        Console.WriteLine("Antamasi luku " + numero + " on negatiivinen ja parillinen.");
+                        Console.WriteLine("Number " + numero + " is negative and even.");
                     }
                     else
                     {
-                        Console.WriteLine("Antamasi luku " + numero + " on negatiivinen ja  pariton.");
+                        Console.WriteLine("Number " + numero + " is negative and odd.");
                     }
                 }
 
             }
             else
             {
-                Console.WriteLine("Et antanut numeroa");
+                Console.WriteLine("You didn't write a number");
             }
 
-            Console.Write("\nAnna kolme numeroa: ");
+            //Task 4. Ask for 3 numbers and arrange them in ascending order
+            Console.Write("\nGive 3 numbers separated with spaces: ");
             string userInput4 = Console.ReadLine();
             string[] numerot = userInput4.Split(' ');
             Array.Sort(numerot);
@@ -103,7 +105,7 @@ namespace ehtorakenne
                 }
                 else
                 {
-                    Console.WriteLine("Et antanut numeroa");
+                    Console.WriteLine("You didn't give a number");
                 }
             }
                 Console.ReadKey();
